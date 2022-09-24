efb2eac060e4917f960d30f6e76a710566e56771
paulo <pauloaugusto@id.uff.br>
2022-09-23 23:33:58 -0300

    making the typeable optional when creating a transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,6 @@
 class Transaction < ApplicationRecord
   belongs_to :account
-  belongs_to :typeable, polymorphic: true
+  belongs_to :typeable, polymorphic: true, optional: true
 
   with_options presence: true do
     validates :value, numericality: true
